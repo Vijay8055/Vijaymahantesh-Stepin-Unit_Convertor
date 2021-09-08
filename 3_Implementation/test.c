a07acea86481a7dd371e4da0fa6d13ab42fb86e8
Vijaymahantesh Bhoosanooramath <35620053+Vijay8055@users.noreply.github.com>
2021-09-08 15:29:50 +0530

    Create test.c

---
+++ 3_Implementation/test.c
@@ -0,0 +1,45 @@
+#include "unity.h"
+#include "record.h"
+#define PROJECT_NAME    "Employee_record_system"
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+FILE *fp;
+void test_Clear_Screen(void){
+  		TEST_ASSERT_EQUAL(0,Clear_Screen());
+}
+// void test_console_color(void){
+//   		TEST_ASSERT_EQUAL(0,console_color(26));
+// }
+void test_create(void){
+         fp=NULL;
+  		TEST_ASSERT_EQUAL(0, create());
+  		TEST_ASSERT_EQUAL(0, create());
+}
+
+void test_update(void){
+  		TEST_ASSERT_EQUAL(0, update());
+  		T
+}
+
+void test_dispaly(void)
+{
+    TEST_ASSERT_EQUAL(0,dispaly());
+}
+
+int  main()
+{
+
+    UNITY_BEGIN();
+
+    RUN_TEST(test_create);
+    //RUN_TEST(test_console_color);
+    RUN_TEST(test_update);
+    RUN_TEST(test_display);
+   ;
+
+    return UNITY_END();
+}
